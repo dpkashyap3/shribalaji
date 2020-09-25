72109d0f0376f18c559f47a6a15e4d1ffdfd154c
dpkashyap3 <dpkashyap3@hotmail.com>
2020-09-25 16:19:43 +0530

    Updates

---
+++ precache-manifest.10bed8af06c88a2e295a10651f3f6942.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b893e40d10b3b4b7c6ed7000c19759",
+    "url": "/shribalaji/index.html"
+  },
+  {
+    "revision": "df3c7b782feb143c0d44",
+    "url": "/shribalaji/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ba5cfbaa9c0cfdc840d6",
+    "url": "/shribalaji/static/css/main.1dc54cfa.chunk.css"
+  },
+  {
+    "revision": "df3c7b782feb143c0d44",
+    "url": "/shribalaji/static/js/2.29c9f33c.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/shribalaji/static/js/2.29c9f33c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba5cfbaa9c0cfdc840d6",
+    "url": "/shribalaji/static/js/main.a5d18b10.chunk.js"
+  },
+  {
+    "revision": "07347921cfaa72fbbe04",
+    "url": "/shribalaji/static/js/runtime-main.2017c705.js"
+  },
+  {
+    "revision": "6ceeceb74756e829045f2c0fbe13b3b9",
+    "url": "/shribalaji/static/media/1.6ceeceb7.jpg"
+  },
+  {
+    "revision": "cce07a121470ba00dba537276d861325",
+    "url": "/shribalaji/static/media/2.cce07a12.jpg"
+  },
+  {
+    "revision": "d3a5c96d304ee73d60f58a06d75c1829",
+    "url": "/shribalaji/static/media/3.d3a5c96d.jpg"
+  },
+  {
+    "revision": "2424cbeb0c3dbd0bd33734a70034aae8",
+    "url": "/shribalaji/static/media/4.2424cbeb.jpg"
+  },
+  {
+    "revision": "f3f8d47d06e6e05b4b4a93f83b3d2def",
+    "url": "/shribalaji/static/media/banner.f3f8d47d.png"
+  },
+  {
+    "revision": "9ba872bfcde74c6cd451f770e8ab3314",
+    "url": "/shribalaji/static/media/info.9ba872bf.png"
+  },
+  {
+    "revision": "f8199f1ca7c6d02597636ede5687ea67",
+    "url": "/shribalaji/static/media/logo.f8199f1c.png"
+  },
+  {
+    "revision": "0a7dceae0b06a8a1c65b1c27fdb71ff9",
+    "url": "/shribalaji/static/media/nigam.0a7dceae.png"
+  },
+  {
+    "revision": "58f1dad048e21c7a3ea2720290459fa4",
+    "url": "/shribalaji/static/media/nitesh.58f1dad0.png"
+  }
+]);
